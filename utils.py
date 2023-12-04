1e5303bd0cf4633441c98c34ee935a649c2fdde1
apostolikas <n.apostolikas@hotmail.com>
2023-12-04 14:17:28 +0100

    Integrated eval into utils

---
+++ utils.py
@@ -0,0 +1,46 @@
+import random
+import re
+
+def add_label(entry):
+
+    ans_value = f'''
+        {entry['answer'].split("####")[0]} The answer is {int(entry['answer'].split('####')[1].replace(',', '',))}
+    '''
+    entry['ans'] = ans_value
+    answer_value = int(entry['answer'].split('####')[1].replace(',', ''))
+    entry['label'] = answer_value
+    
+    return entry
+
+def construct_icl_examples(samples, sample_prompts, number_of_examples):
+
+    icl_examples = random.sample(list(samples), number_of_examples)
+    icl_thinking_styles = random.sample(sample_prompts, number_of_examples)
+    icl_prompt = f''''''
+
+    for i in range(len(icl_examples)):
+
+        icl_prompt += f'''Question: {icl_examples[i]['question']}
+{icl_thinking_styles[i]}
+{icl_examples[i]['ans']}
+'''
+
+    return icl_prompt
+
+
+def evaluate_GSM8K(y_pred, label):
+    pattern = r"\[Answer\]:\s*(.*?)\n(\[Question\]:|$)"
+    match = re.search(pattern, y_pred)
+    if match:
+        y_pred = match.group(1)
+    pred = y_pred.replace(",", "")
+    pred = [s for s in re.findall(r"-?\d+\.?\d*", pred)]
+    if pred == []:
+        return 0
+    pred = pred[-1]
+    pred = pred.replace(",", "").replace(".", "").replace(" ", "")
+    if int(pred) == int(label):
+        return 1
+    else:
+        return 0
+
