2cf059f732f6de0781ecebb26f3254e92b778161
apostolikas <n.apostolikas@hotmail.com>
2024-03-11 17:13:39 +0100

    Prompts moved to separate file

---
+++ prompts.py
@@ -0,0 +1,124 @@
+gsm8k_initial_prompts = [
+    "Let's think step by step",
+    "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and devise a plan. Then, let's carry out the plan, calculate the intermediate results (pay attention to calculation and common sense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Break this down",
+    "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
+    "Let's combine our numerical command and clear thinking to quickly and accurately decipher the answer",
+    #
+    "Let's be very precise and accurate in our calculations",
+    "Let's create a simplified version of the problem to gain insights and test potential solutions",
+    "Embark on a journey to derive the solution to this problem",
+    "Compute the solution with a calculated, stepwise approach",
+    "Let's be very precise and accurate in our calculations",
+    "Our approach will be to methodically work through the problem, ensuring accuracy at each step to derive the correct answer",
+    "Slow down, let's break this down into manageable steps",
+    "Inhale deeply, exhale slowly, and embark on this problem-solving journey with a step-by-step mindset",
+    "Inhale deeply, then utilize a step-by-step logical reasoning process and mathematical tools to deconstruct the problem into manageable components, ultimately leading to a more efficient resolution; let's ensure our calculations are precise and accurate"
+]
+
+aqua_initial_prompts = [
+    "Let's think step by step",
+    "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and make a complete plan.Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Break this down",
+    "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
+    "Let's combine our numerical command and clear thinking to quickly and accurately decipher the answer",
+    #
+    "Let's be very precise and accurate in our calculations",
+    "Let's create a simplified version of the problem to gain insights and test potential solutions",
+    "Embark on a journey to derive the solution to this problem",
+    "Compute the solution with a calculated, stepwise approach",
+    "Let's be very precise and accurate in our calculations",
+    "Our approach will be to methodically work through the problem, ensuring accuracy at each step to derive the correct answer",
+    "Slow down, let's break this down into manageable steps",
+    "Inhale deeply, exhale slowly, and embark on this problem-solving journey with a step-by-step mindset",
+    "Inhale deeply, then utilize a step-by-step logical reasoning process and mathematical tools to deconstruct the problem into manageable components, ultimately leading to a more efficient resolution; let's ensure our calculations are precise and accurate"
+]
+
+
+svamp_initial_prompts = [
+    "Let's think step by step",
+    "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and make a complete plan.Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Break this down",
+    "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
+    "Let's combine our numerical command and clear thinking to quickly and accurately decipher the answer",
+    #
+    "Let's be very precise and accurate in our calculations",
+    "Let's create a simplified version of the problem to gain insights and test potential solutions",
+    "Embark on a journey to derive the solution to this problem",
+    "Compute the solution with a calculated, stepwise approach",
+    "Let's be very precise and accurate in our calculations",
+    "Our approach will be to methodically work through the problem, ensuring accuracy at each step to derive the correct answer",
+    "Slow down, let's break this down into manageable steps",
+    "Inhale deeply, exhale slowly, and embark on this problem-solving journey with a step-by-step mindset",
+    "Inhale deeply, then utilize a step-by-step logical reasoning process and mathematical tools to deconstruct the problem into manageable components, ultimately leading to a more efficient resolution; let's ensure our calculations are precise and accurate"
+]
+
+
+csqa_initial_prompts = [
+    "Let's think step by step",
+    "Let's devise a plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and devise a complete plan.Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Analyze this step by step",    
+    "Start by dissecting the problem into its components, then address each part methodically",
+    "Let's approach this methodically, breaking it into smaller tasks",
+    "Dissect the problem carefully, address each part",
+    "Let's dive in this challenge",#and solve this challenge",
+    # "Let's dive in and solve this challenge step by step",
+    "Approach the problem with a keen eye for detail and methodical precision",
+    "Embark on a quest for understanding, traversing the problem landscape with curiosity and logic",
+    "Your attention to detail here would mean everything",
+    "Please, let's focus and ensure we nail this down",
+]
+
+strategyqa_initial_prompts = [
+    "Let's think step by step",
+    "Let's devise a plan and solve the problem step by step",
+    "Let's first prepare relevant information and make a plan. Then, let's answer the question step by step (pay attention to commonsense and logical coherence)",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Analyze this step by step",    
+    "Start by dissecting the problem into its components, then address each part methodically",
+    "Let's approach this methodically, breaking it into smaller tasks",
+    "Dissect the problem carefully, address each part",
+    "Let's dive in this challenge",#and solve this challenge",
+    # "Let's dive in and solve this challenge step by step",
+    "Approach the problem with a keen eye for detail and methodical precision",
+    "Embark on a quest for understanding, traversing the problem landscape with curiosity and logic",
+    "Your attention to detail here would mean everything",
+    "Please, let's focus and ensure we nail this down",
+]
+
+
+commonsense_mutation_styles = [
+    "The mutation is a variant of the input prompt that introduces a structured thought process.",
+    "The mutation is a variant of the input prompt using unconventional thinking.",
+    "The mutation is a variant of the input prompt that provides an alternative viewpoint.",
+    "The mutation presents a tweaked version of the task, emphasizing logical steps in problem-solving.",
+    "This variant of the prompt, through mutation, offers a fresh perspective on the problem, focusing on strategic thinking.",
+    "Through mutation, the prompt is altered to showcase problem-solving strategies and logical reasoning.",
+    "The mutation introduces a revised version of the prompt, aiming to illuminate the process of logical reasoning and problem-solving.",
+    ]
+
+math_mutation_styles = [
+    "The mutation is a variant of the input prompt that introduces a structured thought process.",
+    # "The mutation is a variant of the input prompt that highlights strategic thought processes and mathematical reasoning.",
+    # "The mutation is a variant of the input prompt that introduces mathematical reasoning and makes it easier to understand.",
+    "The mutation is a variant of the input prompt using unconventional thinking.",
+    "The mutation is a variant of the input prompt that provides an alternative viewpoint.",
+    # "The mutation is a variant of the input prompt that makes it more well-considered and logical.",
+    "The mutation presents a tweaked version of the task, emphasizing logical steps in problem-solving.",
+    "This variant of the prompt, through mutation, offers a fresh perspective on the problem, focusing on strategic thinking.",
+    "Through mutation, the prompt is altered to showcase problem-solving strategies and logical reasoning.",
+    "The mutation introduces a revised version of the prompt, aiming to illuminate the process of logical reasoning and problem-solving.",
+    ]
