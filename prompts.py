590864843bd851d831b706c642df0b1a3fc787ea
apostolikas <n.apostolikas@hotmail.com>
2024-03-18 11:48:38 +0100

    added prompts

--- prompts.py
+++ prompts.py
@@ -19,6 +19,21 @@ gsm8k_initial_prompts = [
     "Inhale deeply, then utilize a step-by-step logical reasoning process and mathematical tools to deconstruct the problem into manageable components, ultimately leading to a more efficient resolution; let's ensure our calculations are precise and accurate"
 ]
 
+gsm8k_inference_prompts = [
+    "",
+    "This is very important to my career",
+    "Let's think step by step",
+    "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and devise a plan. Then, let's carry out the plan, calculate the intermediate results (pay attention to calculation and common sense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Break this down",
+    "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
+    "Let's combine our numerical command and clear thinking to quickly and accurately decipher the answer",
+    # Prompt obtained by evoalg
+    "Focus on strategic thinking to swiftly find the accurate solution"
+]
+
 aqua_initial_prompts = [
     "Let's think step by step",
     "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
@@ -40,11 +55,26 @@ aqua_initial_prompts = [
     "Inhale deeply, then utilize a step-by-step logical reasoning process and mathematical tools to deconstruct the problem into manageable components, ultimately leading to a more efficient resolution; let's ensure our calculations are precise and accurate"
 ]
 
+aqua_inference_prompts = [
+    "",
+    "This is very important to my career",
+    "Let's think step by step",
+    "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and make a complete plan.Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Break this down",
+    "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
+    "Let's combine our numerical command and clear thinking to quickly and accurately decipher the answer",
+    # Prompt obtained by evoalg
+        
+]
+
 
 svamp_initial_prompts = [
     "Let's think step by step",
     "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
-    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and make a complete plan.Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and make a complete plan. Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
     "Let's work this out in a step by step way to be sure we have the right answer",
     "Take a deep breath and work on this problem step-by-step",
     "Break this down",
@@ -59,8 +89,23 @@ svamp_initial_prompts = [
     "Our approach will be to methodically work through the problem, ensuring accuracy at each step to derive the correct answer",
     "Slow down, let's break this down into manageable steps",
     "Inhale deeply, exhale slowly, and embark on this problem-solving journey with a step-by-step mindset",
-    "Inhale deeply, then utilize a step-by-step logical reasoning process and mathematical tools to deconstruct the problem into manageable components, ultimately leading to a more efficient resolution; let's ensure our calculations are precise and accurate"
 ]
+
+svamp_inference_prompts = [
+    "",
+    "This is very important to my career",
+    "Let's think step by step",
+    "Let's first understand the problem and devise a plan to solve the problem. Then, let's carry out the plan and solve the problem step by step",
+    "Let's first understand the problem, extract relevant variables and their corresponding numerals, and make a complete plan. Then, let's carry out the plan, calculate intermediate variables (pay attention to correct numerical calculation and commonsense), solve the problem step by step, and show the answer",
+    "Let's work this out in a step by step way to be sure we have the right answer",
+    "Take a deep breath and work on this problem step-by-step",
+    "Break this down",
+    "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
+    "Let's combine our numerical command and clear thinking to quickly and accurately decipher the answer",
+    # Prompt obtained by evoalg
+
+]
+
 
 
 csqa_initial_prompts = [
@@ -110,7 +155,7 @@ commonsense_mutation_styles = [
     "The mutation introduces a revised version of the prompt, aiming to illuminate the process of logical reasoning and problem-solving.",
     ]
 
-math_mutation_styles = [
+gsm8k_mutation_styles = [
     "The mutation is a variant of the input prompt that introduces a structured thought process.",
     # "The mutation is a variant of the input prompt that highlights strategic thought processes and mathematical reasoning.",
     # "The mutation is a variant of the input prompt that introduces mathematical reasoning and makes it easier to understand.",
@@ -122,3 +167,28 @@ math_mutation_styles = [
     "Through mutation, the prompt is altered to showcase problem-solving strategies and logical reasoning.",
     "The mutation introduces a revised version of the prompt, aiming to illuminate the process of logical reasoning and problem-solving.",
     ]
+
+aqua_mutation_styles = [
+    "The mutation is a variant of the input prompt that introduces a structured thought process.",
+    "The mutation is a variant of the input prompt that provides an alternative viewpoint.",
+    "The mutation presents a tweaked version of the task, emphasizing logical steps in problem-solving.",
+    "This variant of the prompt, through mutation, offers a fresh perspective on the problem, focusing on quickness and strategic thinking.",
+    "Through mutation, the prompt is altered to showcase motivation and logical reasoning.",
+    "The mutation introduces a revised version of the prompt, aiming to focus on logic and swiftness.",
+    "The mutation changes the input prompt to emphasize on precision and quickness.",
+]
+
+svamp_mutation_styles = [
+    "The mutation is a variant of the input prompt that introduces a structured thought process.",
+    "The mutation is a variant of the input prompt that provides an alternative viewpoint.",
+    "The mutation presents a tweaked version of the task, emphasizing logical steps in problem-solving.",
+    "This variant of the prompt, through mutation, offers a fresh perspective on the problem, focusing on quickness and strategic thinking.",
+    "Through mutation, the prompt is altered to showcase motivation and logical reasoning.",
+    "The mutation introduces a revised version of the prompt, aiming to focus on logic and swiftness.",
+]
+
+csqa_mutation_styles = [
+]
+
+strategyqa_mutation_styles = [
+]
