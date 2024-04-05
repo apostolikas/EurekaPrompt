255f6c2a9d1074df19baaeedef485243f7c67da6
apostolikas <n.apostolikas@hotmail.com>
2024-04-05 09:52:17 +0200

    Changed model for semantic clustering

--- compute_uncertainty.py
+++ compute_uncertainty.py
@@ -1,14 +1,29 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 from torch.nn.functional import cosine_similarity
 from itertools import combinations
 import math
+from my_utils import *
+import re
 
 def calculate_entropy(clusters):
     total_responses = sum(len(cluster) for cluster in clusters)
     probabilities = [len(cluster) / total_responses for cluster in clusters]
     entropy_value = -sum(p * math.log2(p) if p != 0 else 0 for p in probabilities)
     return entropy_value
+
+def extract_clusters(input_string):
+    pattern = r'Cluster \d+: \[(.*?)\]'
+    matches = re.findall(pattern, input_string)
+    clusters = [match.strip().split(', ') for match in matches]
+    return clusters
+
+python_string = '''Cluster 1: ['Response 1', 'Response 2', 'Response 4', 'Response 5', 'Response 8', 'Response 9']
+Cluster 2: ['Response 3', 'Response 10']
+Cluster 3: ['Response 6', 'Response 7']'''
+
+clusters = extract_clusters(python_string)
+print(clusters)
 
 
 def extract_responses(filename, model_name):
@@ -49,25 +64,11 @@ def extract_responses(filename, model_name):
         raise NotImplementedError("Model not supported")
 
 def construct_prompt(responses):
-    prompt = "Determine the number of semantic sets in the given responses. The number of semantic sets shows in how many different clusters we could assign these responses to. You need to check whether the responses within the same cluster exactly describe the same thing such as the same entity, digit, or arithmetical results.\n"
+    prompt = "<s> [INST] Determine the number of semantic sets in the given responses. The number of semantic sets shows in how many different clusters we could assign these responses to. You need to check whether the responses within the same cluster exactly describe the same thing such as the same entity, digit, or arithmetical results. Different wording should not affect the clustering as long as the semantics do not change. Give more focus to the entity, the digits, the reasoning steps and the final answer.\n"
     for i, response in enumerate(responses):
         prompt += f"Response {i + 1}: {response}\n"
-    prompt += "Output format: Cluster 1: ['Response 1', 'Response 2', 'Response 3']\n Cluster 2: ['Response 4', 'Response 5', 'Response 6']\n ... \n Cluster N: ['Response M', 'Response N']"
+    prompt += "Output format: Cluster 1: ['Response 1', 'Response 2', 'Response 3']\n Cluster 2: ['Response 4', 'Response 5', 'Response 6']\n ... \n Cluster N: ['Response M', 'Response N'] [/INST]"
     return prompt
-
-
-def get_response(model, tokenizer, input_prompt):
-    input_ids = tokenizer(input_prompt, return_tensors="pt").input_ids.to('cuda')
-    outputs = model.generate(
-        input_ids,
-        max_new_tokens=200,
-        pad_token_id=tokenizer.pad_token_id,
-        eos_token_id=tokenizer.eos_token_id,
-    )
-    response_ids = outputs[0]
-    response_text = tokenizer.decode(response_ids, skip_special_tokens=True)
-    return response_text
-
 
 prompts = [
     "Let's think step by step",
@@ -77,50 +78,51 @@ prompts = [
     "Take a deep breath and work on this problem step-by-step",
     "Break this down",
     "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem",
-    "Embark on a journey to derive the solution to this problem",
-    "Compute the solution with a calculated, stepwise approach",
-    "Let's be very precise and accurate in our calculations",
-    "Our approach will be to methodically work through the problem, ensuring accuracy at each step to derive the correct answer",
-    "Slow down, let's break this down into manageable steps"
+    # "Embark on a journey to derive the solution to this problem",
+    # "Compute the solution with a calculated, stepwise approach",
+    # "Let's be very precise and accurate in our calculations",
+    # "Our approach will be to methodically work through the problem, ensuring accuracy at each step to derive the correct answer",
+    # "Slow down, let's break this down into manageable steps",
+    "Focus on strategic thinking to swiftly find the accurate solution"
 ] 
 
 decode_strategies = ["greedy", "contrastive_search", "multinomial_sampling", "beam_search", "beam_search_with_multinomial_sampling", "top_k_sampling", "top_p_sampling", "sampling0.25", "sampling0.5", "sampling0.75"]
 
-def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[0] 
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-
-def extract_embeddings(model, tokenizer, responses):
-    encoded_input = tokenizer(responses, padding=True, truncation=True, return_tensors='pt').to('cuda')
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    combinations = list(combinations(range(len(responses)), 2))
-    for i, j in combinations:
-        similarity = cosine_similarity(sentence_embeddings[i].unsqueeze(0), sentence_embeddings[j].unsqueeze(0))
-        print(f"Similarity between response {i} and response {j} is {similarity.item()}")
-
-
-tokenizer = AutoTokenizer.from_pretrained("berkeley-nest/Starling-LM-7B-alpha")
-model = AutoModelForCausalLM.from_pretrained("berkeley-nest/Starling-LM-7B-alpha", torch_dtype = torch.float16)
-model = model.to('cuda')
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1", quantization_config=bnb_config, device_map="auto")
 filename = './decode_results.txt'
 answer_dict = extract_responses(filename, 'starling')
+task = 'GSM8K'
+entropy_dict = {}
+file_name = f"./uncertainties/entropy_{task}.txt"
 
 
-for prompt in prompts:
-    for sample in range(len(testset)):
-        responses = []
-        for decode_strategy in decode_strategies:
-            responses.append(answer_dict[prompt][decode_strategy][f'{sample}']) 
-            prompt = construct_prompt(responses)
-            model_input = f'''GPT4 Correct User: {prompt}<|end_of_turn|>GPT4 Correct Assistant:'''
-            response = get_response(model, tokenizer, model_input)
-            response_text = response.split("GPT4 Correct Assistant:")[1]
-
-
-    
-
+with open(file_name, 'w') as file:
+    for prompt in prompts:
+        entropy_list = []
+        for sample in range(1):
+            responses = []
+            for decode_strategy in decode_strategies:
+                responses.append(answer_dict[prompt][decode_strategy][f'{sample}']) 
+            clustering_prompt = construct_prompt(responses)
+            inputs = tokenizer(clustering_prompt, return_tensors="pt").to('cuda')
+            outputs = model.generate(**inputs, max_new_tokens=300, pad_token_id = tokenizer.eos_token_id)
+            output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+            output_text = output_text.split("[/INST]")[1]
+            clusters = extract_clusters(output_text)
+            entropy = calculate_entropy(clusters)
+            entropy_list.append((sample, entropy))
+            print(f"Prompt: {prompt}, Sample: {sample}, Entropy: {entropy}")
+            file.write(f"Prompt: {prompt}, Sample: {sample}, Entropy: {entropy}\n")
+        entropy_dict[prompt] = entropy_list
+        average_entropy = sum(entropy for _, entropy in entropy_list) / len(entropy_list)
+        file.write(f"Average entropy for prompt {prompt}: {average_entropy}\n")
+        print(f"Average entropy for prompt {prompt}: {average_entropy}")
 
 
