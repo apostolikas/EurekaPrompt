2eb4a5557f04cfcbe1d82646ab62d0c7b98c6b11
apostolikas <n.apostolikas@hotmail.com>
2024-04-15 14:36:22 +0200

    compute perplexity

--- perplexity.py
+++ perplexity.py
@@ -2,31 +2,66 @@ import evaluate
 from prompts import *
 from my_utils import *
 
-model_name = "berkeley-nest/Starling-LM-7B-alpha"
+if __name__ == '__main__':
 
-perplexity = evaluate.load("perplexity", module_type="metric")
+    model_name = "berkeley-nest/Starling-LM-7B-alpha"
 
-prompts = gsm8k_inference_prompts
-original_test_dataset = read_jsonl('./data/gsm8k_test.jsonl')
-testset = list(map(add_label, original_test_dataset))
+    perplexity = evaluate.load("perplexity", module_type="metric")
 
-input_dict = {}
+    prompts = gsm8k_inference_prompts
 
-for prompt in prompts:
-    inputs = []
-    for i, sample in enumerate(testset):
-        question = sample['question']
-        input_texts = f"GPT4 Correct User: Question: {question}\nAnswer: {prompt}<|end_of_turn|>GPT4 Correct Assistant:"
-        inputs.append(input_texts)
-    input_dict[prompt] = inputs
+    task = 'gsm8k'
+    num_of_samples = 50 #if task in ['gsm8k','svamp','csqa'] else 35
+    prompts = load_inference_prompts(task)
+    _, testset = load_data(task)
+    random.seed(0)
+    samples = random.sample(testset, num_of_samples)
+
+    input_dict = {}
+
+    for prompt in prompts:
+        inputs = []
+        for i, sample in enumerate(samples):
+            if task == 'gsm8k':
+                question = sample['question']
+                model_input = f'''Question: {question}\nAnswer: {prompt}'''
+                input_prompt = f'''GPT4 Correct User: {model_input}<|end_of_turn|>GPT4 Correct Assistant:'''
+
+            elif task == 'svamp':
+                question = sample['full_question']
+                model_input = f'''Question: {question}\nAnswer: {prompt}'''
+                input_texts = f'''GPT4 Correct User: {model_input}<|end_of_turn|>GPT4 Correct Assistant:'''
+
+            elif task == 'csqa':    
+                question = sample['question']['stem']
+                choices = sample['choice_answers']
+                model_input = f'''Question: {question}\nAnswer Choices: {choices}\nAnswer: {prompt}'''
+                input_prompt = f'''GPT4 Correct User: {model_input}<|end_of_turn|>GPT4 Correct Assistant:'''
+
+            elif task == 'abs_nar':
+                narrative = sample['input']
+                answer_choices = sample['answer_choices']
+                model_input = f'''Question: Can you choose the most related proverb from the list of 5 proverbs given a narrative?\nNarrative: {narrative}\nAnswer choices: {answer_choices}\nAnswer: {prompt}'''
+                input_texts = f'''GPT4 Correct User: {model_input}<|end_of_turn|>GPT4 Correct Assistant:'''
+
+            elif task in ['causal_judg', 'social_iqa', 'date_under', 'sports_und']:
+                question = sample['input']
+                answer_choices = sample['answer_choices']
+                model_input = f'''Question: {question}\nAnswer choices: {answer_choices}\nAnswer: {prompt}'''
+                input_texts = f"GPT4 Correct User: {model_input}<|end_of_turn|>GPT4 Correct Assistant:"
+
+            inputs.append(input_texts)
+        input_dict[prompt] = inputs
 
 
-for prompt, input_texts in input_dict.items():
+    # Write the results to a file
+    with open(f'./perplexities/perplexity_{task}.txt', 'w') as file:
+        for prompt, input_texts in input_dict.items():
+            results = perplexity.compute(model_id=model_name,
+                                        add_start_token=False,
+                                        predictions=input_texts)
+            file.write(f"Prompt: {prompt} | Mean: {results['mean_perplexity']}\n")
 
-    results = perplexity.compute(model_id=model_name,
-                                add_start_token=False,
-                                predictions=input_texts)
-    print(f"Prompt: {prompt} | Mean: {results['mean_perplexity']}")
 
 
 
