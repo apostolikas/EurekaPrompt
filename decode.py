e22afe258f0b6154309750a1125156684c01bf43
apostolikas <n.apostolikas@hotmail.com>
2024-04-16 10:25:51 +0200

    added more out tokens

--- decode.py
+++ decode.py
@@ -26,43 +26,43 @@ def generate_response(task, decode_strategy, model, tokenizer, question, instruc
     inputs = tokenizer(model_input, return_tensors="pt").to('cuda')
 
     if decode_strategy == 'greedy':
-        outputs = model.generate(**inputs, do_sample = False, num_beams = 1, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, do_sample = False, num_beams = 1, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'contrastive_search':
-        outputs = model.generate(**inputs, penalty_alpha=0.6, top_k=4, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, penalty_alpha=0.6, top_k=4, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'multinomial_sampling':
-        outputs = model.generate(**inputs, do_sample=True, num_beams=1, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, do_sample=True, num_beams=1, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'beam_search':
-        outputs = model.generate(**inputs, num_beams=5, do_sample = False, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, num_beams=5, do_sample = False, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'beam_search_with_multinomial_sampling':
-        outputs = model.generate(**inputs, num_beams=5, do_sample=True, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, num_beams=5, do_sample=True, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'top_k_sampling':
-        outputs = model.generate(**inputs, do_sample=True, top_k=50, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, do_sample=True, top_k=50, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
     
     elif decode_strategy == 'top_p_sampling':
-        outputs = model.generate(**inputs, do_sample=True, top_p=0.9, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, do_sample=True, top_p=0.9, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'sampling0.25':
-        outputs = model.generate(**inputs, do_sample=True, top_k = 0, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=0.25)
+        outputs = model.generate(**inputs, do_sample=True, top_k = 0, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=0.25)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
     
     elif decode_strategy == 'sampling0.5':
-        outputs = model.generate(**inputs, do_sample=True, top_k = 0, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=0.5)
+        outputs = model.generate(**inputs, do_sample=True, top_k = 0, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=0.5)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     elif decode_strategy == 'sampling0.75':
-        outputs = model.generate(**inputs, do_sample=True, top_k = 0, max_new_tokens = 400, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=0.75)
+        outputs = model.generate(**inputs, do_sample=True, top_k = 0, max_new_tokens = 600, pad_token_id = tokenizer.pad_token_id, eos_token_id = tokenizer.eos_token_id, temperature=0.75)
         generated_text= tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     else:
@@ -76,13 +76,12 @@ if __name__ == '__main__':
     model = AutoModelForCausalLM.from_pretrained("berkeley-nest/Starling-LM-7B-alpha", device_map='auto', torch_dtype=torch.float16)
     decode_strategies = ["greedy", "contrastive_search", "multinomial_sampling", "beam_search", "beam_search_with_multinomial_sampling", "top_k_sampling", "top_p_sampling", "sampling0.25", "sampling0.5", "sampling0.75"]
 
-    tasks = ['abs_nar']#, 'csqa', 'abs_nar', 'causal_judg', 'social_iqa', 'date_under', 'sports_und']
+    tasks = ['sports_und']#, 'csqa', 'abs_nar', 'causal_judg', 'social_iqa', 'date_under', 'sports_und']
     for task in tasks:
         num_of_samples = 50 #if task in ['gsm8k','svamp','csqa'] else 35
 
         _, testset = load_data(task)
         prompts = load_inference_prompts(task)
-        print(f"Testset is : {type(testset)}")
         random.seed(0)
         samples = random.sample(testset, num_of_samples)
         file_name = f"./decoded/decode_results_{task}.txt"
@@ -90,6 +89,7 @@ if __name__ == '__main__':
         with open(file_name, 'w') as f:
 
             for prompt in prompts:
+                print(f"Decoding for prompt: {prompt}")
                 for decode_strategy in decode_strategies:
                     for i, sample in tqdm(enumerate(samples)):
                         responses = []
@@ -109,13 +109,13 @@ if __name__ == '__main__':
                         elif task == 'abs_nar':
                             narrative = sample['input']
                             choices = sample['answer_choices']
-                            response_text = generate_response(task, decode_strategy, model, tokenizer, question, prompt, choices, narrative)
+                            response_text = generate_response(task, decode_strategy, model, tokenizer, question = None, instruction = prompt, choices = choices, narrative = narrative)
 
                         elif task in ['causal_judg', 'social_iqa', 'date_under', 'sports_und']:
                             question = sample['input']
                             answer_choices = sample['answer_choices']
-                            response_text = generate_response(task, decode_strategy, model, tokenizer, question, prompt, choices)
-                        
+                            response_text = generate_response(task, decode_strategy, model, tokenizer, question = question, instruction = prompt , choices = answer_choices, narrative = None)
+
                         f.write(f"Prompt: {prompt} | Decode Strategy: {decode_strategy} | Sample: {i} | Output: {response_text}\n")
 
 
